6bef1a1433acc7499ce4d9e838e660e10e4748e5
akmaalsyh <taufansyah50@gmail.com>
2025-10-16 16:27:37 +0700

    menambahkan 2 row dalam 1 column

--- app/src/main/java/com/example/questbasiccomposable_236/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_236/Tataletak.kt
@@ -108,8 +108,29 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
         Box(
             modifier = modifier
                 .fillMaxWidth()
-                .height(height =110.dp)
+                .height(height = 110.dp)
                 .background(color = Color.Yellow),
             contentAlignment = Alignment.Center
-        )
+        ) {
+            Column() {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
+                }
+            }
+        }
+    }
 }
