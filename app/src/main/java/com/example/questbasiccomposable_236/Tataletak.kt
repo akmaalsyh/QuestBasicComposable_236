3a3280b4359f4f6b3b28989f75550314178a0a66
akmaalsyh <taufansyah50@gmail.com>
2025-10-16 15:55:02 +0700

    menambah komponen teks nya dalam fungsi TataLetakRow

--- app/src/main/java/com/example/questbasiccomposable_236/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_236/Tataletak.kt
@@ -39,6 +39,9 @@ fun TataLetakRow(modifier: Modifier) {
     Row(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly) {
-
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
     }
 }
